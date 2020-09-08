717d5aa772c4dd130068a688a2165eac92e3a640
BarKoza <BarKoza@users.noreply.github.com>
2020-09-08 14:25:41 +0200

    frontend debut

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ https://templatemo.com/tm-543-breezed
                     <nav class="main-nav">
                         <!-- ***** Logo Start ***** -->
                         <a href="index.html" class="logo">
-                            .ECOPAIN
+                            <u>ECOPAIN</u>
                         </a>
                         <!-- ***** Logo End ***** -->
                         <!-- ***** Menu Start ***** -->
@@ -113,10 +113,9 @@ https://templatemo.com/tm-543-breezed
             <div class="img-fill">
                 <img src="assets/images/slide-01.jpg" alt="">
                 <div class="text-content">
-                  <h3>Bienvue A ECOPAIN</h3>
-                  <h5>Un Trajet Pour La Planet</h5>
-                  <a href="#" class="main-stroked-button">Learn More</a>
-                  <a href="#" class="main-filled-button">Get It Now</a>
+                  <h3>Bienvenue à ECOPAIN</h3>
+                  <h5>Un Trajet Pour La Planète</h5>
+                  <a href="#" class="main-stroked-button">Regarder la Carte<i class="fa fa-arrow-down"></i></a>
                 </div>
             </div>
           </div>
