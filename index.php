c5018007e6afdb5e135c09584085c0fdf0e18646
Casser <paco.hanafi@epsi.fr>
2020-09-09 14:31:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -204,38 +204,36 @@ accessToken: mapboxgl.accessToken
                 <div class="col-lg-4 col-md-4 col-xs-12">
                     <div class="left-text-content">
                         <div class="section-heading">
-                            <h6>Contact Us</h6>
-                            <h2>Feel free to keep in touch with us!</h2>
+                            <h6>Contactez Nous</h6>
                         </div>
                         <ul class="contact-info">
                             <li><img src="assets/images/contact-info-01.png" alt="">010-020-0860</li>
                             <li><img src="assets/images/contact-info-02.png" alt="">ecopainplanet@gmail.com</li>
-                            <li><img src="assets/images/contact-info-03.png" alt="">www.company.com</li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-lg-8 col-md-8 col-xs-12">
                     <div class="contact-form">
-                        <form id="contact" action="" method="get">
+                        <form id="contact" action="" method="post">
                           <div class="row">
                             <div class="col-md-6 col-sm-12">
                               <fieldset>
-                                <input name="name" type="text" id="name" placeholder="Your Name *" required="">
+                                <input name="name" type="text" id="name" placeholder="Ton Nom *" required="">
                               </fieldset>
                             </div>
                             <div class="col-md-6 col-sm-12">
                               <fieldset>
-                                <input name="phone" type="text" id="phone" placeholder="Your Phone" required="">
+                                <input name="phone" type="text" id="phone" placeholder="Téléphone" required="">
                               </fieldset>
                             </div>
                             <div class="col-md-6 col-sm-12">
                               <fieldset>
-                                <input name="email" type="email" id="email" placeholder="Your Email *" required="">
+                                <input name="email" type="email" id="email" placeholder="Ton Email *" required="">
                               </fieldset>
                             </div>
                             <div class="col-md-6 col-sm-12">
                               <fieldset>
-                                <input name="subject" type="text" id="subject" placeholder="Subject">
+                                <input name="subject" type="text" id="subject" placeholder="Sujet">
                               </fieldset>
                             </div>
                             <div class="col-lg-12">
@@ -245,7 +243,7 @@ accessToken: mapboxgl.accessToken
                             </div>
                             <div class="col-lg-12">
                               <fieldset>
-                                <button type="submit" id="form-submit" class="main-button-icon">Send Message Now <i class="fa fa-arrow-right"></i></button>
+                              <button name="Envoyer" type="submit" id="form-submit" class="main-button-icon">Envoyer ton Message <i class="fa fa-arrow-right"></i></button>
                               </fieldset>
                             </div>
                           </div>
@@ -256,6 +254,21 @@ accessToken: mapboxgl.accessToken
         </div>
     </section>
     <!-- ***** Contact Us Area Ends ***** -->
+
+    <?php
+      if (isset ($_POST['Envoyer']))
+      {
+        ini_set( 'display_errors', 1 );
+        error_reporting( E_ALL );
+        $from = $_POST['email'];
+        $to = "ecopainplanet@gmail.com";
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+        $headers = "De :" . $from;
+        mail($to,$subject,$message, $headers);
+        echo "L'email a été envoyé.";
+      }
+    ?>
 
     <!-- ***** Footer Start ***** -->
     <footer>
